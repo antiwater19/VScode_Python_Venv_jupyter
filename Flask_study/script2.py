269a8a9d6001b17d5813aa113a07107cb3ede33f
antiwater19 <minsusu01@gmail.com>
2025-05-23 11:58:23 +0900

    2025-05-23 login page

--- Flask_study/script2.py
+++ Flask_study/script2.py
@@ -2,6 +2,8 @@ from flask import *
 from deptdb import *
 
 app = Flask(__name__)
+# session 사용시에 필수 변수 이거 없으면 에러가 무조건 발생함.
+app.secret_key='admin'
 
 depts = DeptDAO()
 
@@ -27,6 +29,40 @@ def s_list():
 def dept_list():
     depts = DeptDAO().get_depts()
     return render_template('depts.html', dept_list=depts)
+
+@app.route('/cookie') # 쿠키를 설정하는 페이지
+def cookie():
+    res = make_response('<h1>Cookie Set</h1> <a href=\"/cookie/view\">cookie view</a>')
+    res.set_cookie('uid', 'cool')
+    return res
+
+@app.route('/cookie/view') # 쿠키를 읽어오는 페이지
+def cookie_view():
+    cookie_data = request.cookies.get('uid')
+    return cookie_data
+
+# get /login => 로그인 폼
+# post /login => 로그인 처리  
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login/login_form.html') # /templates/login/login_form.html
+    elif request.method == 'POST':
+        # session 이용 사용자 데이터 저장
+        session['login_info']=request.form['email']
+        return redirect(url_for('profile'))
+    else:
+        return redirect(url_for('home'))
+    
+@app.route('/profile')
+def profile():
+    # 로그인 상태 체크
+    if 'login_info' in session:
+        # 로그인 상태
+        email = session['login_info']
+        return render_template('login/profile.html', name=email)
+    else:
+        return '로그인이 필요한 페이지 입니다.'
 
 if __name__ == '__main__':
     app.run(debug=True)
